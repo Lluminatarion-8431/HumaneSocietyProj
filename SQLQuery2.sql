79c5dadc02917ec8d7c1ff345bdae9bc420a3240
KyWilkinson <wilki018@hotmail.com>
2020-09-09 14:13:11 -0500

    Seeded the Category table in the database with Dog, Cat, Rabbit, Ferret, and Parakeet

---
+++ SQLQuery2.sql
@@ -0,0 +1,5 @@
+INSERT INTO dbo.Categories VALUES ('Dog');
+INSERT INTO dbo.Categories VALUES ('Cat');
+INSERT INTO dbo.Categories VALUES ('Rabbit');
+INSERT INTO dbo.Categories VALUES ('Ferret');
+INSERT INTO dbo.Categories VALUES ('Parakeet');
