301a065cfb4b01c5c9570ae3082fc9a19abd8a1f
KyWilkinson <wilki018@hotmail.com>
2020-09-09 15:08:02 -0500

    Seeded Employee table

---
+++ SQLQuery15.sql
@@ -0,0 +1,5 @@
+INSERT INTO dbo.Employees VALUES ('Zidane', 'Trible', 'ZTrible', 'Trance', 1, 'ZTrible@gmail.com');
+INSERT INTO dbo.Employees VALUES ('Tom', 'Riddle', 'TRiddle', 'Avada Kedavra', 2, 'TRiddle@gmail.com');
+INSERT INTO dbo.Employees VALUES ('Harry', 'Dredson', 'HDredson', 'Magic', 3, 'HDredson@gmail.com');
+INSERT INTO dbo.Employees VALUES ('Goku', 'Son', 'GSon', 'Turtle', 4, 'GSon@gmail.com');
+INSERT INTO dbo.Employees VALUES ('Cloud', 'Strife', 'CStrife', 'Fenrir', 5, 'CStrife@gmail.com');
