59d13b2976a87deb18a74edc554c09ccbfaae122
KyWilkinson <wilki018@hotmail.com>
2020-09-09 14:49:04 -0500

    Seeded the diet plan data table

---
+++ SQLQuery9.sql
@@ -0,0 +1,5 @@
+INSERT INTO dbo.DietPlans VALUES ('Dog Diet', 'Dog Food', 2);
+INSERT INTO dbo.DietPlans VALUES ('Cat Diet', 'Cat Food', 2);
+INSERT INTO dbo.DietPlans VALUES ('Rabbit Diet', 'Rabbit Food', 1);
+INSERT INTO dbo.DietPlans VALUES ('Ferret Diet', 'Ferret Food', 1);
+INSERT INTO dbo.DietPlans VALUES ('Parakeet Diet', 'Bird Food', 1);
