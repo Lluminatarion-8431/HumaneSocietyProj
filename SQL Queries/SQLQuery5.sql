ce8ed378f2aad6a46a0187e66337c1325948bd67
KyWilkinson <wilki018@hotmail.com>
2020-09-10 10:01:35 -0500

    Seeded Clients table as well as Adresses Table

---
+++ SQL Queries/SQLQuery5.sql
@@ -0,0 +1,5 @@
+INSERT INTO dbo.Clients VALUES ('Aerith', 'Gainsborough', 'LastCentra', 'Zach2154', 1, 'LastAncient@gmail.com');
+INSERT INTO dbo.Clients VALUES ('Vincent', 'Valentine', 'Chaos', 'Lucretia6547', 2, 'ChaosGunner@gmail.com');
+INSERT INTO dbo.Clients VALUES ('Rufus', 'Shinra', 'TheBoss', 'ShinraHQ45', 3, 'RShinra@gmail.com');
+INSERT INTO dbo.Clients VALUES ('Tifa', 'Lockhart', '7thHeaven', 'Cloud6894', 4, 'TLockhart@gmail.com');
+INSERT INTO dbo.Clients VALUES ('Cid', 'Highwind', 'TheMechanic', 'Shiela6761', 5, 'CHighwind@gmail.com');
---
+++ SQL Queries/SQLQuery6.sql
@@ -0,0 +1,5 @@
+INSERT INTO dbo.Addresses VALUES ('2489 West Dribel LN', 'Waucoma', 43, 46872);
+INSERT INTO dbo.Addresses VALUES ('1957 Memorial Drive', 'Winnipeg', 22, 97628);
+INSERT INTO dbo.Addresses VALUES ('4687 Demolition Ave', 'Chicago', 13, 35894);
+INSERT INTO dbo.Addresses VALUES ('7984 Alligator St', 'Roysan', 9, 57924);
+INSERT INTO dbo.Addresses VALUES ('3541 Grand Ave', 'Tacoma', 36, 45657);
